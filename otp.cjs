8e6fb8186879b4ce32ed9cca19f730c3a6ff9e12
IShubh-10 <shubhamkadam2801@gmail.com>
2026-06-29 21:09:16 +0530

    Fix CORS 0.3

--- otp.cjs
+++ otp.cjs
@@ -21,13 +21,8 @@ const RESEND_COOLDOWN_MS = 30 * 1000;
 const MAX_ATTEMPTS = 5;
 
 app.use(cors({
-    origin: [
-         "https://ishubh-10.github.io",
-        "http://127.0.0.1:5501",
-        "http://localhost:5501",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+    origin: true,
+    credentials: false,
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
