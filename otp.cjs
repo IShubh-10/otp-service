b381cf330f4468e9ee24697b24439628ce19ee13
IShubh-10 <shubhamkadam2801@gmail.com>
2026-06-29 20:06:03 +0530

    backend updated

--- otp.cjs
+++ otp.cjs
@@ -1,10 +1,4 @@
-const path = require("path");
-
-const result = require("dotenv").config({
-    path: path.join(__dirname, ".env")
-});
-
-console.log(result);
+require("dotenv").config();
 
 const express = require("express");
 
